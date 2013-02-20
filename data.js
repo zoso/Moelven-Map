a1b9f5570da9545b8863a72eb21382b85ff1fbed
Lars Lund <larslund@gmail.com>
2013-02-20 15:17:51 +0100

    v2

--- data.js
+++ data.js
@@ -7,8 +7,8 @@
 				"address" : "",
 				"postalnr" : "",
 				"phone": "",
-				"lat" : "",
-				"lng" : ""
+				"lat" : "69.66528588622045",
+				"lng" : "18.990542849999997"
 			},
 			{
 				"name": "Konsern 2",
@@ -41,15 +41,18 @@
 		"data": [
 			{
 				"name": "Moelven Dalaträ AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Industrivägen 2",
+				"postalnr": "SE-785 42 Mockfjärd",
+				"phone":"",
+				"lat": 60.50123902806973,
+				"lng": 14.971547299999997
 			},
 			{
 				"name": "Moelven Norsälven AB",
-				"address": "----",
-				"lat": 59.21294,
-				"lng": 19.15125
+				"address": "Norsälvsvägen 11",
+				"postalnr": "SE-660 50 Vålberg",
+				"lat": 59.397572326849534,
+				"lng": 113.219351200000006
 			},
 			{
 				"name": "Moelven Mjøsbruket AS",
@@ -61,33 +64,43 @@
 			},
 			{
 				"name": "Moelven Årjäng Såg AB",
-				"address": "----",
-				"lat": 57.31294,
-				"lng": 11.45125
+				"address": "",
+				"postalnr":"SE-672 23 Årjäng",
+				"phone":"",
+				"lat": 59.385079047815466,
+				"lng": 12.134091003372992
 			},
 			{
 				"name": "Moelven Valåsen AB",
-				"address": "----",
-				"lat": 59.81294,
-				"lng": 11.25125
+				"address": "",
+				"postalnr" : "SE-69127 Karlskoga",
+				"phone" : "",
+				"lat": 59.346110976532024,
+				"lng": 14.496943999999985
 			},
 			{
 				"name": "Moelven Ransbysågen AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Branäsvägen 35",
+				"postalnr" : "SE-680 60 Sysslebäck",
+				"phone" : "",
+				"lat": 60.67445512826882,
+				"lng": 12.937686800000051
 			},
 			{
 				"name": "Moelven Component AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Skårevägen 60",
+				"postalnr" : "S-650 08 Karlstad",
+				"phone" : "",
+				"lat": 59.43348602688796,
+				"lng": 13.445822399999997
 			},
 			{
 				"name": "Moelven Profil AS",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Industrivegen",
+				"postalnr" : "2260 Kirkenær",
+				"phone" : "",
+				"lat": 60.45324047801497,
+				"lng": 12.067008699999974
 			},
 			{
 				"name": "Moelven Løten AS",
@@ -99,9 +112,11 @@
 			},
 			{
 				"name": "Moelven Telemarksbruket AS (51%)",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Rotebergvegen 21",
+				"postalnr" : "NO-3800 Bø",
+				"phone" : "",
+				"lat": 59.41196962686493,
+				"lng": 9.093444399999953
 			},
 			{
 				"name": "Moelven Numedal AS",
@@ -113,27 +128,43 @@
 			},
 			{
 				"name": "Moelven Notnäs AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "",
+				"postalnr" : "SE-685 25 Torsby",
+				"phone" : "",
+				"lat": 60.1356725216951,
+				"lng": 12.999792638954899
 			},
 			{
 				"name": "Moelven Nössemark Trä AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Rörviken 170",
+				"postalnr" : "SE-668 91 Ed",
+				"phone" : "",
+				"lat": 59.126613126562034,
+				"lng": 11.821661100000028
+			},
+			{
+				"name": "Moelven Våler AS",
+				"address": "Stasjonsveien",
+				"postalnr" : "NO-2435 Braskereidfoss",
+				"phone" : "",
+				"lat": 60.71756912263396,
+				"lng": 11.808212940258723
 			},
 			{
 				"name": "Moelven Tom Heurlin AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "",
+				"postalnr" : "SE-66297 Ånimskog",
+				"phone" : "",
+				"lat": 58.88333303068396,
+				"lng": 12.566667000000052
 			},
 			{
 				"name": "UJ-Trading AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Gransnåret 17",
+				"postalnr" : "SE-711 95 Gusselby",
+				"phone" : "",
+				"lat": 59.6400403271107,
+				"lng": 15.216756199999963
 			}
 		]
 	},
@@ -142,15 +173,19 @@
 		"data" : [
 			{
 				"name": "Moelven Wood AS<br>Moelven Wood AB<br>Moelven Danmark A/S",
-				"address": "----",
+				"address": "",
+				"postalnr" : "",
+				"phone" : "",
 				"lat": 59.91294,
 				"lng": 10.15125
 			},
 			{
 				"name": "Moelven Valåsen Wood AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "",
+				"postalnr" : "SE-691 27 Karlskoga",
+				"phone" : "",
+				"lat": 59.24978770811203,
+				"lng": 15.053088237304676
 			},
 			{
 				"name": "Moelven Are AS",
@@ -170,9 +205,11 @@
 			},
 			{
 				"name": "Moelven Edanesågen AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Arvid Olofssons väg",
+				"postalnr" : "SE-671 70 Edane",
+				"phone" : "",
+				"lat": 59.62463943242501,
+				"lng": 12.817391499999985
 			},
 			{
 				"name": "Moelven Eidsvold Værk AS",
@@ -184,57 +221,75 @@
 			},
 			{
 				"name": "Moelven Østerdalsbruket AS",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Industriveien 133",
+				"postalnr" : "NO-2480 Koppang",
+				"phone" : "",
+				"lat": 61.577964642391514,
+				"lng": 11.03686248465577
 			},
 			{
 				"name": "Moelven List AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Lovene",
+				"postalnr" : "SE-531 96 Lidköping",
+				"phone" : "",
+				"lat": 58.44999998445177,
+				"lng": 13.050000000000068
 			},
 			{
 				"name": "Moelven Notnäs Wood AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Notnäsvägen 9",
+				"postalnr" : "SE-685 33 Torsby",
+				"phone" : "",
+				"lat": 60.12798082764794,
+				"lng": 13.010223200000041
 			},
 			{
 				"name": "Moelven Värmlands Trä AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "",
+				"postalnr" : "SE-661 23 Säffle",
+				"phone" : "",
+				"lat": 59.26688992671029,
+				"lng": 13.029901549999977
 			},
 			{
 				"name": "Moelven Vänerply AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Industrivägen 10",
+				"postalnr" : "SE-547 81 Otterbäcken",
+				"phone" : "",
+				"lat": 58.96386442639153,
+				"lng": 14.026346099999955
 			},
 			{
 				"name": "Moelven Wood Prosjekt AS",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Strandsagvegen 4",
+				"postalnr" : "NO-2383 Brumunddal",
+				"phone" : "",
+				"lat": 60.884024828512544,
+				"lng": 10.931542000000036
 			},
 			{
 				"name": "Moelven Iso3 AS (84,5%)",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Industrivegen 4",
+				"postalnr" : "NO-2390 Moelv",
+				"phone" : "",
+				"lat": 60.93231152856916,
+				"lng": 10.691341950000037
 			},
 			{
 				"name": "Moelven Sør-Tre AS",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Stuttlidalen 2",
+				"postalnr" : "NO-3766 Sannidal",
+				"phone" : "",
+				"lat": 58.90209002632722,
+				"lng": 9.246246799999994
 			},
 			{
 				"name": "Moelven Granvin Bruk AS (87,95%)",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Industrivegen 6",
+				"postalnr" : "NO-5736 Granvin",
+				"phone" : "",
+				"lat": 60.519872749921056,
+				"lng": 6.720003419049143
 			},
 			{
 				"name": "Moelven Soknabruket AS",
@@ -246,21 +301,27 @@
 			},
 			{
 				"name": "Moelven Trysil AS",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Slettmovegen 34",
+				"postalnr" : "NO-2420 Trysil",
+				"phone" : "",
+				"lat": 61.32204882350846,
+				"lng": 12.250915926983694
 			},
 			{
 				"name": "Moelven Van Severen AS",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Pinavegen 5",
+				"postalnr" : "NO-7800 Namsos",
+				"phone" : "",
+				"lat": 64.46242178370554,
+				"lng": 11.50890818465577
 			},
 			{
 				"name": "Moelven Treinteriør AS",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Nordre Kongsveg 56",
+				"postalnr" : "NO-2372 Brøttum",
+				"phone" : "",
+				"lat": 61.00838272955984,
+				"lng": 10.554646257672053
 			},
 			{
 				"name": "Moelven Eidsvoll AS",
@@ -272,9 +333,11 @@
 			},
 			{
 				"name": "Moelven Lovene AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Lovene",
+				"postalnr" : "SE-531 96 Lidköping",
+				"phone" : "",
+				"lat": 58.44999998445177,
+				"lng": 13.050000000000068
 			}
 		]
 	},
@@ -283,31 +346,41 @@
 		"data" : [
 			{
 				"name": "Moelven ByggmModul AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Snickerigatan 1",
+				"postalnr" : "SE-665 33 Kil",
+				"phone" : "",
+				"lat": 59.50781542696781,
+				"lng": 13.300655699999993
 			},
 			{
 				"name": "Moelven Eurowand AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Nastagatan 10 C",
+				"postalnr" : "SE-701 13 Örebro",
+				"phone" : "",
+				"lat": 59.25816592670102,
+				"lng": 15.168217700000014
 			},
 			{
 				"name": "Moelven UK Ltd.",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Vicarage Farm",
+				"postalnr" : "Eastleigh, HANTS SO50 7HD, United Kingdom",
+				"phone" : "",
+				"lat": 50.97645078254901,
+				"lng": -1.3139801598786094
 			},
 			{
 				"name": "Moelven Töreboda AB",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Bruksgatan 8",
+				"postalnr" : "SE-545 21 Töreboda",
+				"phone" : "",
+				"lat": 58.70079812611931,
+				"lng": 14.126765200000023
 			},
 			{
 				"name": "Moelven Elektro AB",
-				"address": "----",
+				"address": "",
+				"postalnr" : "",
+				"phone" : "",
 				"lat": 59.91294,
 				"lng": 10.15125
 			},
@@ -329,9 +402,11 @@
 			},
 			{
 				"name": "Moelven Nordia Prosjekt AS",
-				"address": "----",
-				"lat": 59.91294,
-				"lng": 10.15125
+				"address": "Ensjøveien 3",
+				"postalnr" : "NO-0655 Oslo",
+				"phone" : "",
+				"lat": 59.91634732741296,
+				"lng": 10.7837356
 			},
 			{
 				"name": "Moelven Limtre AS",
